d377e100c2bc2e5a6219aac026c5b6be2fdadff4
black-adm <mattheuscontato17@gmail.com>
2025-04-17 17:40:40 -0300

    fix: setup jest and functional tests config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('node:path');
+
+const root = resolve(__dirname);
+
+module.exports = {
+    rootDir: root,
+    displayName: 'root-tests',
+    testMatch: ['<rootDir>/src/**/*.test.js'],
+    testEnvironment: 'node',
+    clearMocks: true,
+    presets: 'ts-jest',
+    moduleNameMapper: {
+        '@src/(.*)': '<rootDir>/src/$1',
+        '@test/(.*)': '<rootDir>/test/$1',
+    }
+}
